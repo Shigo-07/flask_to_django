5da09c04de9c0ecf9356732754cb50cb27182737
kato shigemichi <shigemichi_kato_0501@yahoo.co.jp>
2022-12-13 22:29:44 +0900

    # 変更内容 Django証券に株券売買履歴のページを追加しました。

--- stock/urls.py
+++ stock/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.StockTopView.as_view(), name='index'),
     path('buy/', views.StockBuy.as_view(), name='buy'),
     path('_buy/', views.stock_buy, name='buy_func'),
+    path('purchase_list/', views.StockPurchaseList.as_view(), name='purchase_list'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import resolve_url, render, redirect
 from django.views.generic import ListView, CreateView
+from django.views import View
 
 from stock.forms import StockPurchaseForm
-from stock.models import Stock
+from stock.models import Stock,StockPurchase
 
 
 class StockTopView(ListView):
@@ -46,3 +47,13 @@ class StockBuy(CreateView):
         context = super().get_context_data(**kwargs)
         context['stocks'] = Stock.objects.all()
         return context
+
+class StockPurchaseList(View):
+    """
+    買取リストを表示する
+    """
+
+    def get(self, request, *args, **kwargs):
+        purchase_list = StockPurchase.objects.all().select_related('stock')
+        context = {'purchase_list': purchase_list}
+        return render(request, 'stock/purchase_list.html', context)
